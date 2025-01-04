9c21f3b16e16829031a05af60c0ffab6ade7564e
Pablowsky <pablocfes@gmail.com>
2025-01-04 17:10:53 -0500

    Changed swagger url

--- seek/urls.py
+++ seek/urls.py
@@ -34,9 +34,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/books/', include('books.urls')),
     path('api/users/', include('users.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
